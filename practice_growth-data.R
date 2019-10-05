ff8dc890f7e26623338c079b00cfebba18c9459d
Robert Johnson <johnson.robert.333@gmail.com>
2019-10-05 14:02:33 -0500

    Practice growth data script

---
+++ practice_growth-data.R
@@ -0,0 +1,11 @@
+# Practice script for fake growth data
+
+library(tidyverse)
+
+
+# data
+data_site = read_csv("data_site.csv")
+data_animal = read_csv("data_site.csv")
+data_growth = read_csv("data_growth.csv")
+
+
